05a990e5b1541f7f62c3456131b7cd3e2c931eac
freddoflamingo <mfrederickebora@gmail.com>
2023-04-11 11:26:11 +0800

    finalize portfolio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className={`container ${classes.header__container}`}>
         <h5>Hello I'm</h5>
         <h1>Fred Ebora</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">Python & Web Developer</h5>
         <CTA />
         <HeaderSocials />
 
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { BsLinkedin } from "react-icons/bs";
 import { FaGithub } from "react-icons/fa";
-import { FiDribbble } from "react-icons/fi";
+import { BsFacebook } from "react-icons/bs";
 import classes from "./Header.module.css";
 
 const HeaderSocials = () => {
   return (
     <div className={classes.header__socials}>
-      <a href="https://linkedin.com" target="_blank" rel="noreferrer">
+      {/* <a href="https://linkedin.com" target="_blank" rel="noreferrer">
         <BsLinkedin />
-      </a>
-      <a href="https://github.com" target="_blank" rel="noreferrer">
+      </a> */}
+      <a
+        href="https://github.com/freddoflamingo"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FaGithub />
       </a>
-      <a href="https://dribbble.com" target="_blank" rel="noreferrer">
-        <FiDribbble />
+      <a
+        href="https://www.facebook.com/freddoflamingo"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsFacebook />
       </a>
     </div>
   );
